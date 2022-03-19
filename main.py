2c6941fcc4f816200da37a9e9d7169bf2a1b7098
abnuo <foo@bar.com>
2022-03-19 20:07:56 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, abort, Response, redirect, jsonify, send_from_directory
+
+app = Flask(__name__, static_folder="/static")
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+app.run(host='0.0.0.0', port=80)
